5c440818a0b08ba9c6bb76e5b8ee6ebd24dd2466
albamatas <albamatasvalls@gmail.com>
2017-03-16 12:03:15 +0100

    actalizado

--- dist/src/js/mobile.js
+++ dist/src/js/mobile.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  var scroll;
 document.getElementById("hamburger").addEventListener ("click", mobilMenu, false);
  function mobilMenu(){
    console.log("clicked");
@@ -6,4 +7,49 @@ document.getElementById("hamburger").addEventListener ("click", mobilMenu, false
  }
  var data = $("a data-slidesjs-item").data;
  console.log(data);
+
+ $('ul.filtros li a').click(function() {
+     var textoFiltro = $(this).text().toLowerCase().replace(/\s/g,"-");
+
+    $("ul.filtros li a").removeClass("filterActive");
+    $(this).addClass("filterActive");
+
+     if(textoFiltro == 'todos')
+     {
+        $('ul.productos li').fadeOut().fadeIn();
+         $('ul.productos li.hidden').fadeIn().removeClass('hidden');
+     }
+     else
+     {
+        $('ul.productos li').fadeOut();
+         $('ul.productos li').each(function()
+         {
+             if(!$(this).hasClass(textoFiltro))
+             {
+                 $(this).fadeOut('normal').addClass('hidden');
+             }
+             else
+             {
+                 $(this).fadeIn('slow').removeClass('hidden');
+             }
+         });
+     }
+     return false;
+     });
+
+/* $(window).scroll(function(){
+   var desktop = 1023;
+   var wWidth=$(window).width();
+
+   if (wWidth > desktop){
+   scrollTop =$(window).scrollTop();
+   bannerT = $(".banner").scrollTop();
+   bannerH = $(".banner").height();
+   console.log(scrollTop);
+   var pixels = scrollTop / 70;
+
+   if(scrollTop < 100 ){
+   }
+ }
+});*/
 });
