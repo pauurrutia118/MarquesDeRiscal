f3cb9d25b596ac1ab03520f0258b2d565f8805d8
albamatas <albamatasvalls@gmail.com>
2017-05-03 17:13:00 +0200

    filtros

--- dist/src/js/mobile.js
+++ dist/src/js/mobile.js
@@ -3,24 +3,22 @@ $( document ).ready(function() {
 document.getElementById("hamburger").addEventListener ("click", mobilMenu, false);
  function mobilMenu(){
    console.log("clicked");
+
    $("ul.nav-menu").toggleClass("visible");
  }
+
  var data = $("a data-slidesjs-item").data;
  console.log(data);
 
  $('ul.filtros__wrap__list li a').click(function() {
      var textoFiltro = $(this).text().toLowerCase().replace(/\s/g,"-");
 
+     $(".sugerencias").hide();
+     $('.filtros__wrap__maridaje :checkbox').attr("disabled", false).prop("checked", false ).parent().css("opacity", 1);
+
     $("ul.filtros__wrap__list li a").removeClass("filterActive");
     $(this).addClass("filterActive");
 
-     if(textoFiltro == 'todos')
-     {
-        $('ul.productos li').fadeOut().fadeIn();
-         $('ul.productos li.hidden').fadeIn().removeClass('hidden');
-     }
-     else
-     {
         $('ul.productos li').fadeOut();
          $('ul.productos li').each(function()
          {
@@ -33,9 +31,64 @@ document.getElementById("hamburger").addEventListener ("click", mobilMenu, false
                  $(this).fadeIn('slow').removeClass('hidden');
              }
          });
-     }
+
+
+     switch (textoFiltro){
+       case "blancos", "rosados":
+                    $('.filtros__wrap__maridaje').find(':checkbox').each(function(){
+                      categoria = $(this).data('id');
+                      if (categoria == "tintos") {
+                        $(this).attr("disabled", true).parent().css("opacity", 0.3);
+                        console.log($(this).parent());
+                      }
+                    });
+                    break;
+
+        case "tintos":
+                        $('.filtros__wrap__maridaje').find(':checkbox').each(function(){
+                         categoria = $(this).data('id');
+                        if (categoria == "blancos") {
+                         $(this).attr("disabled", true).parent().css("opacity", 0.3);
+                           console.log($(this).parent());
+                           }
+                         });
+                        break;
+            }
      return false;
      });
+
+     $('.filtros__wrap__maridaje :checkbox').click(function () {
+       var indice = 0;
+
+       $(".sugerencias").hide();
+       $('.productos > li').hide();
+
+                $('.filtros__wrap__maridaje :checkbox:checked').each(function () {
+
+                    $('.productos > li.' + $(this).attr('rel')).show();
+                    console.log($(this).attr('rel'));
+                    indice = 1;
+                });
+            $('.filtros__wrap__maridaje :checkbox').each(function(){
+              if( $(this).attr('checked') == false) {
+                    indice = indice + 1;
+                }
+              });
+              if (indice == 0){
+                $(".sugerencias").show();
+                $('.productos > li').show();
+
+              }
+
+            });
+     $(".cta").click(function(){
+       $(".filtros").toggleClass("filtros--visible");
+        console.log("clicat");
+     });
+     $(".close").click(function(){
+       $(".filtros").removeClass("filtros--visible");
+     });
+
 
 /* $(window).scroll(function(){
    var desktop = 1023;
